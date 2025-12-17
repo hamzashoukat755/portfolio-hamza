95d37d4ab8d43093dd9a123f4f580328135e5953
hamzashoukat755 <hamzashoukat125@gmail.com>
2025-12-17 19:20:17 +0500

    Portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com"
+                href="https://github.com/hamzashoukat755"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
